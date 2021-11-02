180a46596594d625205b6b699d13b823bf7ed1af
Jooyul Yoon <juyeolyoon@gmail.com>
2021-11-01 21:31:52 -0700

    frontend

--- frontend/app/src/Components/Main.js
+++ frontend/app/src/Components/Main.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
-import beers from "./beers"
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import beers from "./Product";
 
 class Main extends Component {
-    render(){
-        return(
-            <div>
-                {/*Render Different Component based on Route*/}
-                <Route path="/" component={beers}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/*Render Different Component based on Route*/}
+        <Route path="/product" component={beers} />
+      </div>
+    );
+  }
 }
 
 export default Main;
