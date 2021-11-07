1a59eeea81a6313ecf109f9df91124b0f794928c
Bobosuen <47465944+ybhbo@users.noreply.github.com>
2021-11-07 10:29:14 -0800

    Add controller to support simple add and get all products

---
+++ src/main/java/edu/sjsu/enterprise/grocery/controller/GroceryController.java
@@ -0,0 +1,31 @@
+package edu.sjsu.enterprise.grocery.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.sjsu.enterprise.grocery.Product;
+import edu.sjsu.enterprise.grocery.ProductRepository;
+
+@Controller
+@RequestMapping(path="/product")
+public class GroceryController {
+  @Autowired
+  private ProductRepository productRepository;
+
+  @PostMapping(path="/add", consumes = "application/json")
+  public @ResponseBody String addNewUser (@RequestBody Product product) {
+    productRepository.save(product);
+    return "Saved";
+  }
+
+  @GetMapping(path="/all", produces = "application/json")
+  public @ResponseBody Iterable<Product> getAllUsers() {
+    // This returns a JSON or XML with the users
+    return productRepository.findAll();
+  }
+}
